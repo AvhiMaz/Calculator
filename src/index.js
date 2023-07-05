06b534e31ef57f051bdfd83b49d5d54bda0555bf
Pawank06 <pawan06kumar2003@gmail.com>
2023-07-05 23:27:54 +0530

    added brain

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert(hey);
+function myFunction() {
+    let num1 = document.getElementById("numOne").value;
+    let Opera = document.getElementById("Op").value;
+    let num2 = document.getElementById("numTwo").value;
+
+    console.log(num1, Opera, num2);
+
+    
+}
