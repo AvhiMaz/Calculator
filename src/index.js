f31f0fdab3200baaf857f84f30a2d0332fa7bd51
AvhiMaz <avhimazumder5@outlook.com>
2023-07-06 00:00:36 +0530

    modified js file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,30 @@ function myFunction() {
     let Opera = document.getElementById("Op").value;
     let num2 = document.getElementById("numTwo").value;
 
-    console.log(num1, Opera, num2);
+    //console.log(num1, Opera, num2);   
+    let ans;
+    switch (Opera) {
+        case '+':
+            ans = parseInt(num1) + parseInt(num2);
+            document.getElementById('demo').innerHTML = ans;
+            break;
+        
+        case '-':
+            ans = num1 - num2;
+            document.getElementById('demo').innerHTML = ans;
+            break;
+        
+        case '*':
+            ans = num1 * num2;
+            document.getElementById('demo').innerHTML = ans;
+            break;
 
-    
+        case '/':
+            ans = num1 / num2;
+            document.getElementById('demo').innerHTML = ans;
+            break;
+
+        default:
+            break;
+    }
 }
